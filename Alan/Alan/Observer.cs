a638bfe118e94d854d483dfb1fcd00ede102cfd1
Ascendzor <wow.troymare@gmail.com>
2014-09-01 21:07:20 +1200

    screenshot going

---
+++ Alan/Alan/Observer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using Tesseract;
+using System.Drawing.Imaging;
+
+namespace Alan
+{
+    class Observer
+    {
+        public Observer()
+        {
+
+        }
+
+        public void Observe()
+        {
+            GenerateImage();
+        }
+
+        private void GenerateImage()
+        {
+            var bounds = Screen.PrimaryScreen.Bounds;
+            using (var bmp = new Bitmap(bounds.Width, bounds.Height, PixelFormat.Format32bppArgb))
+            {
+                using (var gfx = Graphics.FromImage(bmp))
+                {
+                    gfx.CopyFromScreen(bounds.X, bounds.Y, 0, 0, bounds.Size, CopyPixelOperation.SourceCopy);
+                    bmp.Save("shot.png");
+                }
+            }
+        }
+    }
+}
---
+++ Alan/Alan/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Alan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Observer leObserver = new Observer();
+
+            while(true)
+            {
+                leObserver.Observe();
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
